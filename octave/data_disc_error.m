452b21928d79be1b7dd3f90c45b73a2eb83d0440
PyRadiolarus <60543948+PyRadiolarus@users.noreply.github.com>
2021-02-05 20:58:17 +0900

    Octave 009

--- octave/data_disc_error.m
+++ octave/data_disc_error.m
@@ -11,11 +11,11 @@ tt = [1:(days)];
 plot(tt, temp)
 
 #凡例作成
-h=legend({"Temprature"},"Location","bestoutside") #凡例位置はプロットにかぶらない最適な位置で、グラフの外側を指定
+h=legend({"Temperature"},"Location","bestoutside") #凡例位置はプロットにかぶらない最適な位置で、グラフの外側を指定
 set(h,"FontName","Times New Roman","FontSize",11)
 set(gca,"FontName","Times New Roman","FontSize",11)
 xlabel("Number of days elapsed since July 1, 2019(days)")
-ylabel("Temprature(degree Celsius)")
+ylabel("Temperature(degree Celsius)")
 
 #プロットをpngファイルに出力
 print("Yamagata2019-21_disc_er","-dpng")
